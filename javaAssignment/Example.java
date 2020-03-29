451fd17f0ed6ae9dddb81a71d950add126e35f2c
Church-FullStack <62269553+Church-FullStack@users.noreply.github.com>
2020-03-29 16:22:52 -0700

    Add files via upload

---
+++ javaAssignment/Example.java
@@ -0,0 +1,23 @@
+package javaAssignment;
+
+public class Example {
+
+	public static void main(String[] args) {
+		String name;
+		int age;
+		int salary;
+		
+		salary = 200;
+		name = "Church";
+		age = 99;
+		
+		System.out.println("Name:" + " " + name + "\n" + "Age:" + " " + age + "\n" + "Salary:" + " " + salary);
+
+	}
+	}
+
+
+
+
+
+
